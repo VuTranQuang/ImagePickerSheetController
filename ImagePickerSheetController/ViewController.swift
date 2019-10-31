89363a83626f2259c10c89d8396d046357f62f0b
VuTranQuang <vutran7993@gmail.com>
2019-10-31 08:48:54 +0700

    chọn ảnh đại diện từ photolibary

--- ImagePickerSheetController/ViewController.swift
+++ ImagePickerSheetController/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageFace: UIImageView!
+    
+    private var profileImage: UIImage!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onClickChooseImage(_ sender: UITapGestureRecognizer) {
+        
+        let authorization = PHPhotoLibrary.authorizationStatus()
+        if authorization == .notDetermined {
+            PHPhotoLibrary.requestAuthorization { _ in
+                DispatchQueue.main.async {
+                    self.onClickChooseImage(sender)
+                }
+            }
+        }
+        if authorization == .authorized {
+            let controller = ImagePickerSheetController()
+            controller.addAction(action: ImageAction(title: NSLocalizedString("Take Photo or Video", comment: "Action Title"), secondaryTitle: NSLocalizedString("Use this one", comment: "Action Title"), handler: { _ in
+                self.presentCamera()
+            }, secondaryHandler: { action, numberOfPhotos in
+                controller.getSelectedImagesWithCompletion(completion: { images in
+                    self.profileImage = images[0]
+                    self.imageFace.image = self.profileImage
+                })
+            }))
+            controller.addAction(action: ImageAction(title: NSLocalizedString("Cancel", comment: "Action Title"), style: .Cancel, handler: nil, secondaryHandler: nil))
+            self.present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    func presentCamera()
+    {
+        print("拍照")
+    }
 }
 
